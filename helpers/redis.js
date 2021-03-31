3b4dbd0c5abdf93f0db335823eb2ead0632bfc92
Rifki Fauzi <kubido@gmail.com>
2021-03-31 13:49:08 +0700

    fix redis auth url error

--- helpers/redis.js
+++ helpers/redis.js
@@ -3,7 +3,7 @@ let redis;
 if (process.env.REDISTOGO_URL) {
   let rtg = new URL(process.env.REDISTOGO_URL)
   redis = require("redis").createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(":")[1]);
+  redis.auth(rtg.password);
 } else {
   redis = require("redis").createClient();
 }
