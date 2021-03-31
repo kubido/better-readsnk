e26a57ca9b926b703c95d7a0db72d30dbf3cc6ff
Rifki Fauzi <kubido@gmail.com>
2021-03-31 10:18:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000
+const LATEST_CHAPTER = process.env.LATEST_CHAPTER
+
+
+const fetchAndModifiy = (url, options = {}) => {
+  axios.get(url)
+    .then(resp => {
+      let htmlData = resp.data
+        .replace(/<img/g, "<img loading=\"lazy\" width=\"1600\" height=\"1124\"")
+        .replace(/href\=\"\/css/g, 'href="https://ww7.readsnk.com/css')
+        .replace(/src\=\"\/js/g, 'href="https://ww7.readsnk.com/js')
+        .replace(/https:\/\/ww[0-7]+.readsnk.com\/chapter\/shingeki-no-kyojin-chapter-/g, `${options.gshost}/chapter/`)
+      options.resolve(htmlData)
+    })
+    .catch(err => {
+      options.reject({ url, error: err.message })
+    })
+}
+
+// Middleware
+// Make request to readsnk.com based on request.url
+app.use((req, res, next) => {
+  let url = req.url
+  if (url === "/") {
+    url = "https://ww7.readsnk.com"
+  } else {
+    const chapterId = +url.match(/\d+/)[0]
+    url = `https://ww7.readsnk.com/chapter/shingeki-no-kyojin-chapter-${chapterId}`
+    if (chapterId >= LATEST_CHAPTER) url = "https://ww7.readsnk.com/not%20found"
+  }
+
+  // Fetch html content and manipulate DOM
+  try {
+    fetchAndModifiy(url, {
+      host: req.protocol + '://' + req.get('host'),
+      resolve: (htmlData) => res.send(htmlData),
+      reject: (err) => res.send(err)
+    })
+  } catch (error) {
+    res.send({ error: err.message })
+  }
+})
+
+// APP ROUTES
+app.get("/", (req, res) => res.send(req.htmlData))
+app.get('/chapter/:chapterId', (req, res) => res.send(req.htmlData))
+
+app.listen(PORT, () => console.log('server started at port ' + PORT))
---
+++ readme.md
@@ -0,0 +1,8 @@
+# About
+
+readsnk.com with native lazy loading images
+
+# How to
+
+`npm install`
+`node app.js`
